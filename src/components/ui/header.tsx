aede53d50a0d3fa4e465b4a58073f10ae77418f7
serafim <matheus18serafim@gmail.com>
2023-11-18 12:50:16 -0300

    feat: add header menu using sheet components

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,36 +1,69 @@
 import {
   CarIcon,
   CarrotIcon,
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
   MenuIcon,
+  PercentIcon,
   ShoppingBagIcon,
   ShoppingCartIcon,
+  SignalIcon,
 } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
-import { Sheet, SheetContent, SheetTrigger } from "./sheet";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const Header = () => {
   return (
     <Card className="flex justify-between items-center p-8">
       <Sheet>
-        <SheetTrigger>
+        <SheetTrigger asChild>
           <Button variant="outline" size="icon">
             <MenuIcon />
           </Button>
         </SheetTrigger>
 
-        <SheetContent>
-          <Button variant="outline" size="icon">
-            <CarrotIcon />
-          </Button>
+        <SheetContent side="left">
+          <SheetHeader className="text-lg font-semibold">Menu</SheetHeader>
 
-          <Button variant="outline" size="icon">
-            <CarIcon />
-          </Button>
+          <div className="mt-2 flex flex-col gap-4">
+            <Button
+              variant="outline"
+              size="icon"
+              className="w-full gap-2 justify-start px-2"
+            >
+              <LogInIcon size={16} />
+              Fazer login
+            </Button>
 
-          <Button variant="outline" size="icon">
-            <ShoppingBagIcon />
-          </Button>
+            <Button
+              variant="outline"
+              size="icon"
+              className="w-full gap-2 justify-start px-2"
+            >
+              <HomeIcon size={16} />
+              Inicio
+            </Button>
+
+            <Button
+              variant="outline"
+              size="icon"
+              className="w-full gap-2 justify-start px-2"
+            >
+              <PercentIcon size={16} />
+              Ofertas
+            </Button>
+
+            <Button
+              variant="outline"
+              size="icon"
+              className="w-full gap-2 justify-start px-2"
+            >
+              <ListOrderedIcon size={16} />
+              Catálogo
+            </Button>
+          </div>
         </SheetContent>
       </Sheet>
 
@@ -38,9 +71,16 @@ const Header = () => {
         GOOD <span>Suplementos</span>
       </h1>
 
-      <Button variant="outline" size="icon">
-        <ShoppingCartIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon">
+            <ShoppingCartIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="right">
+          <SheetHeader className="text-lg font-semibold">Carrinho</SheetHeader>
+        </SheetContent>
+      </Sheet>
     </Card>
   );
 };
