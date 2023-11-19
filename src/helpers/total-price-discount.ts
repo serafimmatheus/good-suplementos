9fec55c7c9c919884bf9df333f596a4fdcd08a7b
serafim <matheus18serafim@gmail.com>
2023-11-19 08:10:02 -0300

    feat: add function helper for calculate basePrice more discount percentage

---
+++ src/helpers/total-price-discount.ts
@@ -0,0 +1,23 @@
+import { Product } from "@prisma/client";
+
+interface ProductWithTotalPrice extends Product {
+  totalPrice: number;
+}
+
+const totalPriceDiscount = (product: Product): ProductWithTotalPrice => {
+  if (product.discountPercentage > 0) {
+    const discount =
+      (Number(product.basePrice) * product.discountPercentage) / 100;
+    return {
+      ...product,
+      totalPrice: Number(product.basePrice) - discount,
+    };
+  } else {
+    return {
+      ...product,
+      totalPrice: Number(product.basePrice),
+    };
+  }
+};
+
+export { totalPriceDiscount };
