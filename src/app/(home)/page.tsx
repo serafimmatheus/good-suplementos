59ef3438524b54719e82f60080461384f1f94cd6
serafim <matheus18serafim@gmail.com>
2023-11-19 08:10:23 -0300

    feat: add ProductPage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,36 @@
 import Image from "next/image";
 import Categories from "./components/categories";
+import { prismaClient } from "@/lib/prisma";
+import ProductList from "./components/product-list";
 
-export default function Home() {
+export default async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
   return (
-    <main className="px-5 py-8">
+    <main className="py-8">
       <Image
         src="/banner-home-desconto.png"
         alt="até 55% de desconto esse mês"
         width={0}
         height={0}
         sizes="100vw"
-        className="w-full h-auto"
+        className="w-full h-auto px-5"
       />
 
-      <div className="mt-8">
+      <div className="mt-8 px-5 ">
         <Categories />
+      </div>
+
+      <div className="mt-8">
+        <h2 className="text-base font-bold mb-4 px-5 ">Ofertas da semana</h2>
+
+        <ProductList products={deals} />
       </div>
     </main>
   );
