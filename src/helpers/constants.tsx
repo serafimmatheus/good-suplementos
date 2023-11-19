b3145c9eeb3c59ed012b6a1021d56c238e24bdd0
serafim <matheus18serafim@gmail.com>
2023-11-19 18:36:02 -0300

    feat: add constants add icons

---
+++ src/helpers/constants.tsx
@@ -0,0 +1,16 @@
+import {
+  HeadphonesIcon,
+  KeyboardIcon,
+  MonitorIcon,
+  MouseIcon,
+  SpeakerIcon,
+} from "lucide-react";
+
+export const CATEGORY_ICON = {
+  keyboards: <KeyboardIcon size={16} />,
+  monitors: <MonitorIcon size={16} />,
+  headphones: <HeadphonesIcon size={16} />,
+  speakers: <SpeakerIcon size={16} />,
+  mouses: <MouseIcon size={16} />,
+  mousepads: <MouseIcon size={16} />,
+};
