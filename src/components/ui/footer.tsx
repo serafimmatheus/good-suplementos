33bb17b6c6f29c19d7cf01038094858b62079422
serafim <matheus18serafim@gmail.com>
2023-11-19 09:12:42 -0300

    feat: add max-width

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-accent py-8 px-5">
+      <p className="text-sm opacity-70 max-w-5xl mx-auto">
+        © 2023 Copyright GOOD Suplementos
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -14,7 +14,6 @@ import { Card } from "./card";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
-import { Separator } from "./separator";
 
 const Header = () => {
   const { data: session, status } = useSession();
@@ -28,104 +27,108 @@ const Header = () => {
   };
 
   return (
-    <Card className="flex justify-between items-center p-8">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="outline" size="icon">
-            <MenuIcon />
-          </Button>
-        </SheetTrigger>
+    <Card className="py-8 px-5">
+      <div className="max-w-5xl mx-auto flex justify-between items-center ">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
 
-        <SheetContent side="left">
-          <SheetHeader className="text-lg font-semibold">Menu</SheetHeader>
+          <SheetContent side="left">
+            <SheetHeader className="text-lg font-semibold">Menu</SheetHeader>
 
-          <div className="mt-2 flex flex-col gap-4">
-            {status === "authenticated" && session?.user && (
-              <div className="flex flex-clo">
-                <div className="flex item-center gap-2 my-4">
-                  <Avatar>
-                    <AvatarFallback>
-                      {session.user.name![0].toUpperCase()}
-                    </AvatarFallback>
-                    {session.user.image && (
-                      <AvatarImage src={session.user.image!} />
-                    )}
-                  </Avatar>
+            <div className="mt-2 flex flex-col gap-4">
+              {status === "authenticated" && session?.user && (
+                <div className="flex flex-clo">
+                  <div className="flex item-center gap-2 my-4">
+                    <Avatar>
+                      <AvatarFallback>
+                        {session.user.name![0].toUpperCase()}
+                      </AvatarFallback>
+                      {session.user.image && (
+                        <AvatarImage src={session.user.image!} />
+                      )}
+                    </Avatar>
 
-                  <div className="flex flex-col">
-                    <p className="font-medium">{session.user.name}</p>
-                    <span className="text-sm opacity-60">Boas compras!</span>
+                    <div className="flex flex-col">
+                      <p className="font-medium">{session.user.name}</p>
+                      <span className="text-sm opacity-60">Boas compras!</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            )}
+              )}
 
-            {status === "authenticated" ? (
+              {status === "authenticated" ? (
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="w-full gap-2 justify-start px-2"
+                  onClick={handleLogOut}
+                >
+                  <LogOutIcon size={16} />
+                  Sair
+                </Button>
+              ) : (
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="w-full gap-2 justify-start px-2"
+                  onClick={handleLogin}
+                >
+                  <LogInIcon size={16} />
+                  Fazer login
+                </Button>
+              )}
+
               <Button
                 variant="outline"
                 size="icon"
                 className="w-full gap-2 justify-start px-2"
-                onClick={handleLogOut}
               >
-                <LogOutIcon size={16} />
-                Sair
+                <HomeIcon size={16} />
+                Inicio
               </Button>
-            ) : (
+
               <Button
                 variant="outline"
                 size="icon"
                 className="w-full gap-2 justify-start px-2"
-                onClick={handleLogin}
               >
-                <LogInIcon size={16} />
-                Fazer login
+                <PercentIcon size={16} />
+                Ofertas
               </Button>
-            )}
 
-            <Button
-              variant="outline"
-              size="icon"
-              className="w-full gap-2 justify-start px-2"
-            >
-              <HomeIcon size={16} />
-              Inicio
+              <Button
+                variant="outline"
+                size="icon"
+                className="w-full gap-2 justify-start px-2"
+              >
+                <ListOrderedIcon size={16} />
+                Catálogo
+              </Button>
+            </div>
+          </SheetContent>
+        </Sheet>
+
+        <h1>
+          GOOD <span>Suplementos</span>
+        </h1>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <ShoppingCartIcon />
             </Button>
-
-            <Button
-              variant="outline"
-              size="icon"
-              className="w-full gap-2 justify-start px-2"
-            >
-              <PercentIcon size={16} />
-              Ofertas
-            </Button>
-
-            <Button
-              variant="outline"
-              size="icon"
-              className="w-full gap-2 justify-start px-2"
-            >
-              <ListOrderedIcon size={16} />
-              Catálogo
-            </Button>
-          </div>
-        </SheetContent>
-      </Sheet>
-
-      <h1>
-        GOOD <span>Suplementos</span>
-      </h1>
-
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="outline" size="icon">
-            <ShoppingCartIcon />
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="right">
-          <SheetHeader className="text-lg font-semibold">Carrinho</SheetHeader>
-        </SheetContent>
-      </Sheet>
+          </SheetTrigger>
+          <SheetContent side="right">
+            <SheetHeader className="text-lg font-semibold">
+              Carrinho
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
     </Card>
   );
 };
