2639d83198bbcb3d6f77b286641937434b7032ad
serafim <matheus18serafim@gmail.com>
2023-11-26 11:04:27 -0300

    feat: add private route in page MeusPedidos

--- src/app/meus-pedidos/page.tsx
+++ src/app/meus-pedidos/page.tsx
@@ -4,6 +4,7 @@ import { prismaClient } from "@/lib/prisma";
 import { PackageSearchIcon } from "lucide-react";
 import { getServerSession } from "next-auth";
 import OrderItem from "./components/order-item";
+import { RoutePrivate } from "@/components/ui/RoutePrivate";
 
 const MeusPedidos = async () => {
   const user = getServerSession(authOptions);
@@ -26,21 +27,23 @@ const MeusPedidos = async () => {
   });
 
   return (
-    <div className="max-w-5xl mx-auto px-5 py-8">
-      <Badge
-        variant="outline"
-        className="gap-2 px-4 py-2 text-sm uppercase border-2 border-primary"
-      >
-        <PackageSearchIcon />
-        Meus Pedidos
-      </Badge>
+    <RoutePrivate>
+      <div className="max-w-5xl mx-auto px-5 py-8">
+        <Badge
+          variant="outline"
+          className="gap-2 px-4 py-2 text-sm uppercase border-2 border-primary"
+        >
+          <PackageSearchIcon />
+          Meus Pedidos
+        </Badge>
 
-      <div className="flex flex-col gap-5 mt-8">
-        {orders.reverse().map((order) => (
-          <OrderItem key={order.id} order={order}></OrderItem>
-        ))}
+        <div className="flex flex-col gap-5 mt-8">
+          {orders.reverse().map((order) => (
+            <OrderItem key={order.id} order={order}></OrderItem>
+          ))}
+        </div>
       </div>
-    </div>
+    </RoutePrivate>
   );
 };
 
---
+++ src/components/ui/RoutePrivate.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const RoutePrivate = ({ children }: { children: React.ReactNode }) => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (status === "unauthenticated" || !session) {
+      router.push("/"); // Redireciona para a página inicial se não houver uma sessão autenticada
+    }
+  }, [status, session, router]);
+
+  return (
+    <>
+      {status === "unauthenticated" && null}
+      {status === "authenticated" && children}
+    </>
+  );
+};
+
+export { RoutePrivate };
