4a5e0ef5ae10bfea187f3438a9342386af82b362
serafim <matheus18serafim@gmail.com>
2023-12-02 12:33:17 -0300

    fix: bug in pedidos page

--- src/app/meus-pedidos/page.tsx
+++ src/app/meus-pedidos/page.tsx
@@ -1,52 +1,52 @@
-// import { Badge } from "@/components/ui/badge";
-// import { authOptions } from "@/lib/auth";
-// import { prismaClient } from "@/lib/prisma";
-// import { PackageSearchIcon } from "lucide-react";
-// import { getServerSession } from "next-auth";
-// import OrderItem from "./components/order-item";
-// import { RoutePrivate } from "@/components/ui/RoutePrivate";
+import { Badge } from "@/components/ui/badge";
+import { authOptions } from "@/lib/auth";
+import { prismaClient } from "@/lib/prisma";
+import { PackageSearchIcon } from "lucide-react";
+import { getServerSession } from "next-auth";
+import OrderItem from "./components/order-item";
+import { RoutePrivate } from "@/components/ui/RoutePrivate";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
-// const MeusPedidos = async () => {
-//   const user = getServerSession(authOptions);
+const MeusPedidos = async () => {
+  const user = getServerSession(authOptions);
 
-//   if (!user) {
-//     return <p>Acesso Negado!</p>;
-//   }
+  if (!user) {
+    return <p>Acesso Negado!</p>;
+  }
 
-//   const orders = await prismaClient.order.findMany({
-//     where: {
-//       userId: (user as any).id,
-//     },
-//     include: {
-//       orderProduct: {
-//         include: {
-//           product: true,
-//         },
-//       },
-//     },
-//   });
+  const orders = await prismaClient.order.findMany({
+    where: {
+      userId: (user as any).id,
+    },
+    include: {
+      orderProduct: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
 
-//   return (
-//     <RoutePrivate>
-//       <div className="max-w-5xl mx-auto px-5 py-8">
-//         <Badge
-//           variant="outline"
-//           className="gap-2 px-4 py-2 text-sm uppercase border-2 border-primary"
-//         >
-//           <PackageSearchIcon />
-//           Meus Pedidos
-//         </Badge>
+  return (
+    <RoutePrivate>
+      <div className="max-w-5xl mx-auto px-5 py-8">
+        <Badge
+          variant="outline"
+          className="gap-2 px-4 py-2 text-sm uppercase border-2 border-primary"
+        >
+          <PackageSearchIcon />
+          Meus Pedidos
+        </Badge>
 
-//         <div className="flex flex-col gap-5 mt-8">
-//           {orders.reverse().map((order) => (
-//             <OrderItem key={order.id} order={order}></OrderItem>
-//           ))}
-//         </div>
-//       </div>
-//     </RoutePrivate>
-//   );
-// };
+        {/* <div className="flex flex-col gap-5 mt-8">
+          {orders.reverse().map((order) => (
+            <OrderItem key={order.id} order={order}></OrderItem>
+          ))}
+        </div> */}
+      </div>
+    </RoutePrivate>
+  );
+};
 
-// export default MeusPedidos;
+export default MeusPedidos;
